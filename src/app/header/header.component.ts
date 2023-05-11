75f82a456d8773033ef4d34dd77c5515bc8ff2fb
squidawg <78506807+squidawg@users.noreply.github.com>
2023-05-11 16:52:42 +0400

    feat: add method to to open dialog window for create board button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,32 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthenticationService} from "../authentication/authentication.service";
 import {Subscription} from "rxjs";
+import {DialogService} from "../shared/dialog/dialog.service";
+import {CreateBoardComponent} from "../board/dialog/create-board/create-board.component";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit, OnDestroy{
   private userSub!: Subscription;
-  isAuth = false
-  constructor(private authService: AuthenticationService) {
-  }
+  isAuth = false;
+
+  constructor(private authService: AuthenticationService, private dialogService: DialogService) {}
+
   ngOnInit() {
     this.userSub = this.authService.user.subscribe(user => {
-      this.isAuth = !!user
+      this.isAuth = !!user;
     });
   }
+
   ngOnDestroy() {
     this.userSub.unsubscribe();
+  }
+
+  onCreateBoard(){
+    this.dialogService.openDialog(CreateBoardComponent);
   }
 }
