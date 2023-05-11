c9af1f6ffa2ddd7575352e08b96edeec95f4e879
squidawg <78506807+squidawg@users.noreply.github.com>
2023-05-11 16:50:42 +0400

    feat: add logic for delete board

--- src/app/shared/dialog/delete-warning-form/delete-warning-form.component.ts
+++ src/app/shared/dialog/delete-warning-form/delete-warning-form.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {DashboardService} from "../../../dashboard/dashboard.service";
+import {BoardService} from "../../../board/board.service";
+import {DialogService} from "../dialog.service";
 
 @Component({
   selector: 'app-delete-warning-form',
   templateUrl: './delete-warning-form.component.html',
   styleUrls: ['./delete-warning-form.component.css']
 })
-export class DeleteWarningFormComponent {
+export class DeleteWarningFormComponent{
 
+  constructor(private boardService: BoardService,
+              private dashboardService: DashboardService,
+              public dialog: DialogService) {}
+
+  boardId = '';
+
+  onSubmit(){
+    this.boardId = this.dashboardService.boardId
+    this.dashboardService.deleteBoard(this.boardId)
+  }
 }
