22d9654b5d94b9f23ae6c53794ae78a9fd3cd5af
squidawg <78506807+squidawg@users.noreply.github.com>
2023-05-15 16:58:00 +0400

    feat: add  logic  for Dashboard component

---
+++ src/app/dashboard/dashboard-storage.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {DashboardModel} from "./dashboard.model";
+import {AuthenticationService} from "../authentication/authentication.service";
+import {DashboardService} from "./dashboard.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DashboardStorageService {
+  constructor(private dashboardService: DashboardService,
+              private http: HttpClient,
+              private userData: AuthenticationService) {}
+    boards!: DashboardModel[]
+    boardId = '';
+    bearerToken = new HttpHeaders()
+      .set('Authorization', `Bearer ${this.userData.user.value.token}`);
+
+    fetchBoards() {
+    this.http.get<DashboardModel[]>('https://final-task-backend-test.up.railway.app/boards',
+        { headers: this.bearerToken })
+        .subscribe(resData => {
+            this.dashboardService.setBoards(resData);
+        });
+    }
+
+    createBoard(title:string, owner:string) {
+      this.boards = this.dashboardService.getBoards();
+      this.http.post<DashboardModel>('https://final-task-backend-test.up.railway.app/boards',{
+          title: title,
+          owner: owner,
+          users: []
+      }, { headers: this.bearerToken } )
+          .subscribe( resData => {
+              this.boards.push(resData);
+              this.dashboardService.setBoards(this.boards.slice())
+              this.dashboardService.boardsChanged.next(this.boards.slice())
+        })
+
+    }
+
+    deleteBoard(id:string) {
+        this.boards = this.dashboardService.getBoards();
+        return this.http.delete<DashboardModel>(`https://final-task-backend-test.up.railway.app/boards/${id}`,
+            {headers: this.bearerToken})
+        .subscribe( resData => {
+            const index = this.boards.indexOf(resData)
+            this.boards.splice(index,1)
+            this.dashboardService.setBoards(this.boards.slice())
+            this.dashboardService.boardsChanged.next(this.boards.slice())
+    });
+  }
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
-import {Component, DoCheck, OnInit} from '@angular/core';
-import {BoardService} from "../board/board.service";
-import {DashboardModel} from "./dashboard.model";
+import {Component, DoCheck, OnDestroy, OnInit} from '@angular/core';
 import {DashboardService} from "./dashboard.service";
+import {DashboardModel} from "./dashboard.model";
+import {DashboardStorageService} from "./dashboard-storage.service";
 import {DialogService} from "../shared/dialog/dialog.service";
 import {DeleteWarningFormComponent} from "../shared/dialog/delete-warning-form/delete-warning-form.component";
+import {BoardService} from "../board/board.service";
+import {Router} from "@angular/router";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard',
@@ -11,28 +14,40 @@ import {DeleteWarningFormComponent} from "../shared/dialog/delete-warning-form/d
   styleUrls: ['./dashboard.component.css']
 })
 
-export class DashboardComponent implements OnInit, DoCheck{
+export class DashboardComponent implements OnInit, OnDestroy {
   constructor(
       private dialogService: DialogService,
+      private dashboardStorageService: DashboardStorageService,
       private dashboardService: DashboardService,
-      private boardService: BoardService) {}
+      private boardService: BoardService,
+      private router: Router) {}
 
+  subscription!: Subscription;
   boards!: DashboardModel[];
 
   ngOnInit() {
     this.onFetch();
-  }
-
-  ngDoCheck() {
-    this.boards = this.boardService.getBoards();
+    this.subscription = this.dashboardService.boardsChanged
+        .subscribe((columnData: DashboardModel[] ) => {
+          this.boards = columnData;
+        });
   }
 
   onFetch(){
-    this.dashboardService.fetchBoards();
+    this.dashboardStorageService.fetchBoards();
   }
 
-  onDelete(id: number){
-    this.dashboardService.boardId = this.boards[id]._id;
+  onLoadBoard(i:number){
+    this.boardService.setBoardId(i);
+    this.router.navigate(['/board']);
+  }
+
+  onDelete(id: number) {
+    this.dashboardStorageService.boardId = this.boards[id]._id;
     this.dialogService.openDialog(DeleteWarningFormComponent);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
   }
 }
