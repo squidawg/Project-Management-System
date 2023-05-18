d64919d5fea48fd1a8d9b95d06101fd46c5624c4
squidawg <78506807+squidawg@users.noreply.github.com>
2023-05-18 18:13:38 +0400

    feat: implement logic for edit profile route

--- src/app/authentication/edit-profile/edit-profile.component.ts
+++ src/app/authentication/edit-profile/edit-profile.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {FormControl} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthenticationService} from "../authentication.service";
 
 @Component({
@@ -7,12 +7,31 @@ import {AuthenticationService} from "../authentication.service";
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.css']
 })
-export class EditProfileComponent {
+export class EditProfileComponent implements OnInit{
   constructor(private authentication: AuthenticationService) {
   }
+  isLoading = false;
+  editUserForm!: FormGroup;
   hide = true;
+  ngOnInit() {
+    this.editUserForm = new FormGroup({
+      'name': new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      'login': new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      'password': new FormControl(null, [Validators.required, Validators.minLength(4)])
+    });
+  }
 
-  onError(value: FormControl, valueName: string) {
-    return this.authentication.getErrorMessage(value, valueName)
+  onError(value: any, valueName: string) {
+    return this.authentication.getErrorMessage(value, valueName);
+  }
+
+  onSubmit(){
+    const name = this.editUserForm.value.name;
+    const login = this.editUserForm.value.login;
+    const password = this.editUserForm.value.password;
+    this.isLoading = true;
+    this.authentication.editUser(name, login, password).subscribe(resData => {
+      this.isLoading = false;
+    });
   }
 }
