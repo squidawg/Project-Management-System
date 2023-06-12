d06e6b556e794bdfc28d4046c9324ed1b159c978
squidawg <78506807+squidawg@users.noreply.github.com>
2023-06-12 15:02:09 +0400

    feat: add custom pipe to transform data in template

---
+++ src/app/shared/tranform.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {UserAssignService} from "./user-assign.service";
+
+@Pipe({
+    name: 'transform',
+    pure: false
+})
+export class TranformPipe implements PipeTransform {
+    constructor(private userAssignService: UserAssignService) {}
+
+    transform(key: string) {
+        const users = this.userAssignService.getUsers()
+        return users.map(user => key.includes(user._id)? user.name: '').join('')
+    }
+}
